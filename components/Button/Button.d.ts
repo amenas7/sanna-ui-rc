03d0eaa1483e213fb3d55df314b475300c340a5d
amenas7 <amenas94@gmail.com>
2026-04-19 15:34:42 -0500

    build: sanna-ui-rc v0.1.2

---
+++ components/Button/Button.d.ts
@@ -0,0 +1,12 @@
+import { default as React } from 'react';
+
+type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'outline-primary' | 'outline-secondary' | 'outline-success' | 'outline-danger' | 'outline-warning' | 'outline-info' | 'outline-light' | 'outline-dark';
+type ButtonSize = 'sm' | 'md' | 'lg';
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariant;
+    size?: ButtonSize;
+    loading?: boolean;
+    fullWidth?: boolean;
+}
+export declare const Button: React.FC<ButtonProps>;
+export {};
